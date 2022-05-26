9bf3bccb4d07669defa52174bd4559bb0a2e446c
Oryam <48219965+or-yam@users.noreply.github.com>
2022-05-26 10:57:38 +0300

    Add animations options

--- bin/webiya-cli.js
+++ bin/webiya-cli.js
@@ -1,3 +1,3 @@
 #!/usr/bin/env node
 
-import '../src/index.js';
+import '../lib/index.js';
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,25 @@
-function main() {
-  console.log('Hello Webiya!');
+import chalkAnimation from 'chalk-animation';
+import inquirer from 'inquirer';
+
+const sleep = (ms: number = 2000) => new Promise(resolve => setTimeout(resolve, ms));
+
+const options = ['rainbow', 'pulse', 'glitch', 'radar', 'neon', 'karaoke'] as const;
+type animation = typeof options[number];
+
+async function main() {
+  const answers = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'color',
+      message: 'What color do you want to use?',
+      choices: options
+    }
+  ]);
+
+  const { color } = answers;
+  const animation = chalkAnimation[color as animation]('Hello Webiya!');
+  await sleep();
+  animation.stop();
 }
 
 main();
